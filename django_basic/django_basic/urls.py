c6360bb8fe62b8e9a379442e1f56a40515113069
Jeon_minGyu <coby4313@gmail.com>
2022-06-24 01:57:21 +0900

    static 복습

--- django_basic/django_basic/urls.py
+++ django_basic/django_basic/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
-from myapp import views
+import myapp.views
+import staticapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.first),
-    path('second/', views.second),
+    path('', staticapp.views.home),
+    path('st', staticapp.views.home),
+    path('dd', myapp.views.first),
+    path('second/', myapp.views.second),
+     
     path('products/', include('product.urls')),
-    path('board/', include('product.urls'))
+    path('board/', include('product.urls')),
+    
+   
+    
 ]
